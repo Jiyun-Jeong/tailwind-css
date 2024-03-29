1e0400bd4647227253c754ae90472126db4ad02d
Jiyun Jeong <rijamong0830@naver.com>
2024-03-29 14:19:58 +0900

    tailwind install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-  },
-  variants: {
-    extend: {
-      borderWidth: ['hover'],
-    },
-  },
   plugins: ['prettier-plugin-tailwindcss'],
 }
 
